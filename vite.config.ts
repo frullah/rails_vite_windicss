0460ab75e22e741a81031ffc9e2e0fa6a7a87a33
Fajarullah <frullah12@gmail.com>
2021-07-28 23:56:51 +0700

    set windi configFiles

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,16 @@ import { defineConfig } from 'vite'
 import WindiCSS from 'vite-plugin-windicss'
 import RubyPlugin from 'vite-plugin-ruby'
 import FullReload from 'vite-plugin-full-reload'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
     WindiCSS({
       root: __dirname,
-      // scan: {
-      //   fileExtensions: ['erb', 'js'],
-      //   dirs: ['app/views', 'app/frontend'], // or app/javascript, or app/packs
-      // },
-      preflight: false
+      // to tell the config files to be loaded
+      // without it, the config file cannot be loaded
+      configFiles: [path.resolve("windi.config.ts")]
     }),
     FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 800, always: false })
   ],
